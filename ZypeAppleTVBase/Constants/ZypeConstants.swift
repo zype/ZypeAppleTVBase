c1207c5c59399b8fe9e13dbccf220e4b0c939522
Andrey Kasatkin <a@svetliy.com>
2016-05-03 14:18:03 -0400

    move all files from SDK

---
+++ ZypeAppleTVBase/Constants/ZypeConstants.swift
@@ -0,0 +1,12 @@
+//
+//  ZypeConstants.swift
+//  Zype
+//
+//  Created by Ilya Sorokin on 10/20/15.
+//  Copyright © 2015 Eugene Lizhnyk. All rights reserved.
+//
+
+//dev
+let kAppKey = "ZSko8msvWHy7NG28ZqGUqzWytLfp7S3zg1bX-WgY-5jM39dX7bIiTgWyVN_goKiZ"
+let kOAuthClientSecret = ""
+let kOAuthClientId = ""
