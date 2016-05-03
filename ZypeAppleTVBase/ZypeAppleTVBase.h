b7da15c553c3a31f6701af5d1907ff7a187704f4
Andrey Kasatkin <a@svetliy.com>
2016-05-03 10:19:10 -0400

    Initial Commit

---
+++ ZypeAppleTVBase/ZypeAppleTVBase.h
@@ -0,0 +1,19 @@
+//
+//  ZypeAppleTVBase.h
+//  ZypeAppleTVBase
+//
+//  Created by Andrey Kasatkin on 5/3/16.
+//  Copyright © 2016 Zype. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZypeAppleTVBase.
+FOUNDATION_EXPORT double ZypeAppleTVBaseVersionNumber;
+
+//! Project version string for ZypeAppleTVBase.
+FOUNDATION_EXPORT const unsigned char ZypeAppleTVBaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZypeAppleTVBase/PublicHeader.h>
+
+
